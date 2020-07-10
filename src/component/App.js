0e2d82e18de49a1987d637f235cbbcd6f02a2c05
Aviatrix_ui <varshamodake@gmail.com>
2020-07-10 19:01:27 +0100

    Initial Commit

--- src/component/App.js
+++ src/component/App.js
@@ -1,42 +1,12 @@
 import React ,{ Component } from 'react';
-import Projects from './Projects';
-import SocialProfile from './SocialProfiles';
-import ProfilePicture from '../assets/profile.png';
-import Title from './Title';
-import Jokes from './Joke';
-import Header from './Header';
+
 class App extends Component{
-    state={bioDisplay:false};
-    
-        toggleDisplayBio=()=>
-        {
-            this.setState({ bioDisplay: !this.state.bioDisplay});
-           
-        }
+  
     render()
     {
         return(
             <div>
-                <img src={ProfilePicture} alt='profile' className='profile'></img>
-                <h1>Hello !</h1>
-                <p>My name is varsha.</p>
-                {this.state.bioDisplay ? <Title/>:null}
-                <p>I am always looking forward to working on meaningful projects.</p>
-                { this.state.bioDisplay?(
-                <div>
-                    <p>I live in London, and code every day.</p>
-                    <p>My Favorite language is JavaScript, and I think React Js is awesome. </p>
-                    <p>Beside coding I also like to cook and watch a movie.</p>
-                    <button onClick={this.toggleDisplayBio}>show less</button>
-                </div>) : (<div>
-                <button onClick={this.toggleDisplayBio} >Read More</button></div>)
-                }
-                <hr />
-                <Projects />
-                <hr />
-            <SocialProfile />
-            <hr />
-            <Jokes />
+               <h1>React App!!!</h1>
             </div>
         )
     }
